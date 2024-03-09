91963b1c2bb2edbbb00227e60e8ff6555cc32a0f
14-sep-full-stack-code <raykarsiddhesh14@gmail.com>
2024-03-09 07:24:12 +0530

    form added

--- myforms/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MyForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100)
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(label='Your Message', widget=forms.Textarea)
--- myforms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import my_form_view, success_view
-
-urlpatterns = [
-    path('my-form/', my_form_view, name='my_form'),
-    path('success/', success_view, name='success'),
-]
